38d7c52290955296f4e5d1cc8aa5e16d8f6863c3
Sergey2677 <sergeyasdf2677@icloud.com>
2022-11-02 06:32:43 +0300

    -

---
+++ Personal/project.py
@@ -0,0 +1,34 @@
+# Your project must have a main function and three or more additional functions.
+# At least three of those additional functions must be accompanied by tests that can be executed with pytest.
+# which should be in the “root” (i.e., top-level folder) of your project.
+# Your 3 required custom functions other than main must also be in project.py
+# and defined at the same indentation level as main (i.e., not nested under any classes or functions).
+# Your test functions must be in a file called test_project.py, which should also be in the “root” of your project
+# Be sure they have the same name as your custom functions, prepended with test_ (test_custom_function, for example,
+# where custom_function is a function you’ve implemented in project.py).
+# You are welcome to implement additional classes and functions as you see fit beyond the minimum requirement.
+# Any pip-installable libraries that your project requires must be listed, one per line,
+# in a file called requirements.txt in the root of your project.
+
+
+# Do not use this tool on a host that you don't have permission to test!
+
+
+def main():
+    ...
+
+
+def function_1():
+    ...
+
+
+def function_2():
+    ...
+
+
+def function_n():
+    ...
+
+
+if __name__ == "__main__":
+    main()
