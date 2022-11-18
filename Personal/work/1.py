8ec5014beac225978b38423d551507c5a58aaa15
Sergey2677 <sergeyasdf2677@icloud.com>
2022-11-18 20:09:14 +0300

    Modifier

--- Personal/work/1.py
+++ Personal/work/1.py
@@ -1,7 +1,9 @@
 # 1. Посчитайте сумму всех четных и нечетных чисел в интервале от 100 до 1000
 
 def counter(number_from: int, number_up_to: int) -> int:
-    return sum([i for i in range(number_from, number_up_to) if i % 2 == 0])
+    odd = sum([i for i in range(number_from, number_up_to) if i % 2 == 0])
+    even = sum([i for i in range(number_from, number_up_to) if i % 2 == 1])
+    return odd + even
 
 
 print(counter(1, 10))
